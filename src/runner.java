c443db91bf9c0b97437abefed17b7a67ea25b792
jacksonnerdcamp <jacksonjjacobs@gmail.com>
2020-02-09 05:07:48 -0500

    working on lorentz transformation library

--- src/runner.java
+++ src/runner.java
@@ -2,8 +2,7 @@ public class runner {
     public static void main(String[] args){
         double[] xVals = new double[50000];
         hyperboLib h1 = new hyperboLib();
-        h1.pHyperbola(-1,1);
-        System.out.println(h1.getX_1() + ", " + h1.getY_1());
-        System.out.println(Math.log(Math.E));
+        h1.finalMotorLocation(3,2);
+        System.out.println(h1.getF());
     }
 }
